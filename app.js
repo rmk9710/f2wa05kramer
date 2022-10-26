8392869a43bb234b50fe9a259ba2f6d940964c72
Reece Kramer <54494919+rmk9710@users.noreply.github.com>
2022-10-26 18:13:48 -0500

    assignment 9

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+
+var mydata = require('./routes/mydata');
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use('/mydata', mydata);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
